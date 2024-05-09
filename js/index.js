1ce5374680065d6db722eda8d2baaf6459f00680
JeremiahID <jeremiahefe.je@gmail.com>
2024-05-09 17:28:14 +0100

    scripting added

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 const Hamburger = document.querySelector('#hamburger');
 const CloseHamburger = document.querySelector('#close-hamburger');
+const Navigation = document.querySelector('.navigation');
+console.log("working")
+
+Hamburger.addEventListener('click', ()=>{
+    if(Navigation.style.display === "none"){
+        Navigation.style.display = "block";
+
+    }else{
+        Navigation.style.display = "none";
+    }
+})
