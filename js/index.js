9a125816799bad248e7b2acfc0935deb7465d935
JeremiahID <jeremiahefe.je@gmail.com>
2024-05-09 17:44:03 +0100

    the tab layout set

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 const Hamburger = document.querySelector('#hamburger');
 const CloseHamburger = document.querySelector('#close-hamburger');
-const Navigation = document.querySelector('.navigation');
+const Navigation = document.querySelector('#navigation');
 console.log("working")
 
 Hamburger.addEventListener('click', ()=>{
     if(Navigation.style.display === "none"){
         Navigation.style.display = "block";
+
+    }else{
+        Navigation.style.display = "none";
+    }
+})
+
+
+CloseHamburger.addEventListener('click', ()=>{
+    if(Navigation.style.display === "block"){
+        Navigation.style.display = "none";
 
     }else{
         Navigation.style.display = "none";
