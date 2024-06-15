1322222c09a0b3c52185de8e9c51fd82ce94d46e
JeremiahID <jeremiahefe.je@gmail.com>
2024-06-15 13:28:03 +0100

    Jquery scripting added

--- js/index.js
+++ js/index.js
@@ -1,23 +1,47 @@
-const Hamburger = document.querySelector('#hamburger');
-const CloseHamburger = document.querySelector('#close-hamburger');
-const Navigation = document.querySelector('#navigation');
-console.log("working")
+// Vanilla Js
+// const Hamburger = document.querySelector('#hamburger');
+// const CloseHamburger = document.querySelector('#close-hamburger');
+// const Navigation = document.querySelector('#navigation');
+// console.log("working")
 
-Hamburger.addEventListener('click', ()=>{
-    if(Navigation.style.display === "none"){
-        Navigation.style.display = "block";
+// Hamburger.addEventListener('click', ()=>{
+//     if(Navigation.style.display === "none"){
+//         Navigation.style.display = "block";
 
-    }else{
-        Navigation.style.display = "none";
+//     }else{
+//         Navigation.style.display = "none";
+//     }
+// })
+
+
+// CloseHamburger.addEventListener('click', ()=>{
+//     if(Navigation.style.display === "block"){
+//         Navigation.style.display = "none";
+
+//     }else{
+//         Navigation.style.display = "none";
+//     }
+// })
+
+// JQuery js
+const Hamburger = $("#hamburger");
+const CloseHamburger = $("#close-hamburger");
+const Navigation = $("#navigation");
+
+
+// adding event listerner to the hamburger icon
+Hamburger.click(()=>{
+    if(Navigation.css("display") === "none"){
+        Navigation.css("display", Navigation.delay(200).fadeIn())
+    } else{
+        Navigation.css("display", "none")
     }
 })
 
-
-CloseHamburger.addEventListener('click', ()=>{
-    if(Navigation.style.display === "block"){
-        Navigation.style.display = "none";
-
-    }else{
-        Navigation.style.display = "none";
+CloseHamburger.click(()=>{
+    if(Navigation.css("display") === "block"){
+        Navigation.css("display", Navigation.delay(400).fadeOut())
+    } else{
+        Navigation.css("display", "none")
     }
 })
